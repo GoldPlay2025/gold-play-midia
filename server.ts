d541f1331d418aa2540bc7fc2ef8412b0a2e13a8
Gold Play <goldplaynews@gmail.com>
2026-07-25 18:11:20 -0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -17,7 +17,7 @@ async function startServer() {
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, x-cron-secret');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, x-cron-secret, x-api-key, apiKey, *');
     if (req.method === 'OPTIONS') {
       res.sendStatus(200);
       return;
